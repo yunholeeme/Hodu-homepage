dcddad6f1b2ee8fe0cf4f965a5a325f7fffc8f42
hodu <neuezeal@gmail.com>
2019-12-10 15:47:09 +0900

    feat: add mirror function to index.js
    add mirror function to index.js using webRTC

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
 const title = document.querySelector(".title");
 title.innerText = "❤ Hodu's Homepage";
 
-function init() {
+function mirror() {
+    const width = 320;
+    const height = 240;
+    let video = document.getElementById("videoinput"); // video is the id of video tag
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(function(stream) {
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(function(err) {
+        console.log("An error occurred! " + err);
+    });
+}
 
+function init() {
+    mirror();
 }
 
 init();
