fb6b26b5e78626facd645973a709f89d0b57c508
hodu <neuezeal@gmail.com>
2019-12-10 15:24:36 +0900

    feat: add clock.js
    🛑 add clock.js

---
+++ clock.js
@@ -0,0 +1,16 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h2");
+
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}`:hours}:${minutes < 10 ? `0${minutes}`:minutes}:${seconds < 10 ? `0${seconds}`:seconds}`;
+}
+
+function init() {
+    setInterval(getTime, 1000);
+}
+
+init();
