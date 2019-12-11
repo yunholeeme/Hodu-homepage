75fc06a35e4091cb85b2cce792a57cb59bbf295d
hodu <neuezeal@gmail.com>
2019-12-11 21:24:46 +0900

    feat: add to store username
    add to store username

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const title = document.querySelector(".title");
 title.innerText = "❤ Hodu's Homepage";
 
+const width = 320;
+const height = 240;
+
 function mirror() {
-    const width = 320;
-    const height = 240;
+
     let video = document.getElementById("videoinput"); // video is the id of video tag
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(function(stream) {
         video.srcObject = stream;
         video.play();
@@ -16,8 +18,17 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     });
 }
 
+// function in_video() {
+//     let src = new cv.Mat(height, width, cv.CV_8UC4);
+//     let dst = new cv.Mat(height, width, cv.CV_8UC1);
+//     let cap = new.VideoCapture("images/20191207_152022.mp4");
+    
+//     console.log(cap);
+// }
+
 function init() {
     mirror();
+    // in_video();
 }
 
 init();
